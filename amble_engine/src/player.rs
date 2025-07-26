8a96a3463048ac90ae29a42388966ae80fdf4603
David Brown <dave@pop-top.local>
2025-07-26 17:14:40 -0500

    New Flag type implemented and replaced Player flags field with HashSet<Flag>. New type will allos simple fixed-string type flags as before, but add flag sequences to support multi-step puzzles.

--- amble_engine/src/player.rs
+++ amble_engine/src/player.rs
@@ -1,6 +1,7 @@
 //! Player -- module for a player in Amble
 use crate::{ItemHolder, Location, WorldObject};
 
+use log::{info, warn};
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use uuid::Uuid;
@@ -13,7 +14,7 @@ pub struct Player {
     pub description: String,
     pub location: Location,
     pub inventory: HashSet<Uuid>,
-    pub flags: HashSet<String>,
+    pub flags: HashSet<Flag>,
     pub score: usize,
 }
 impl Default for Player {
@@ -25,7 +26,7 @@ impl Default for Player {
             description: "default".into(),
             location: Location::default(),
             inventory: HashSet::<Uuid>::default(),
-            flags: HashSet::<String>::default(),
+            flags: HashSet::<Flag>::default(),
             score: 1,
         }
     }
@@ -59,4 +60,99 @@ impl ItemHolder for Player {
     fn contains_item(&self, item_id: Uuid) -> bool {
         self.inventory.contains(&item_id)
     }
+}
+
+/// Flags that can be applied to the player
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Hash)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum Flag {
+    Simple {
+        name: String,
+    },
+    Sequence {
+        name: String,
+        #[serde(default)]
+        step: u8,
+        #[serde(default)]
+        end: Option<u8>,
+    },
+}
+impl Flag {
+    /// Return string value of the flag.
+    pub fn value(&self) -> String {
+        match self {
+            Self::Simple { name } => name.to_string(),
+            Self::Sequence { name, step, .. } => format_sequence_value(name, *step),
+        }
+    }
+
+    /// Advances to next step of a sequence
+    ///
+    /// Logs a warning and does nothing if called on a simple flag.
+    /// Returns the resulting sequence number or None
+    pub fn advance(&mut self) -> Option<u8> {
+        match self {
+            Flag::Simple { name, .. } => {
+                warn!("advance() called on non-sequence flag '{name}'");
+                None
+            },
+            Flag::Sequence { name, step, end } => {
+                if let Some(final_step) = end {
+                    *step = std::cmp::min(*step + 1, *final_step);
+                } else {
+                    *step += 1;
+                }
+                info!("sequence '{name}' advanced to step {step}");
+                Some(*step)
+            },
+        }
+    }
+
+    /// Resets to beginning of sequence
+    pub fn reset(&mut self) {
+        match self {
+            Flag::Simple { name } => warn!("reset() called on non-sequence flag '{name}'"),
+            Flag::Sequence { name, step, .. } => {
+                *step = 0;
+                info!("sequence '{name}' reset to step '{step}'")
+            },
+        }
+    }
+
+    /// Create a new simple flag
+    pub fn simple(name: &str) -> Flag {
+        Flag::Simple { name: name.to_string() }
+    }
+
+    /// Create a new sequence flag
+    pub fn sequence(name: &str, end: Option<u8>) -> Flag {
+        Flag::Sequence {
+            name: name.to_string(),
+            step: 0u8,
+            end,
+        }
+    }
+
+    /// Get base name of the flag
+    pub fn name(&self) -> &str {
+        match self {
+            Flag::Simple { name } => name,
+            Flag::Sequence { name, .. } => name,
+        }
+    }
+}
+impl std::fmt::Display for Flag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Flag::Simple { name } => write!(f, "{name}"),
+            Flag::Sequence { name, step, .. } => write!(f, "{}#{}", name, step),
+        }
+    }
+}
+
+/// Formats a sequence-type flag into a string value
+///
+/// Format is <name>#<step>, e.g. "hal_reboot#2"
+pub fn format_sequence_value(name: &str, step: u8) -> String {
+    format!("{name}#{step}")
 }
